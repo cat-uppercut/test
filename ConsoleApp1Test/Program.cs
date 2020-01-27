0b526b9a3ea4af38dd0680ccc24526926eed3911
dku <dmytro.kunakh@gmail.com>
2020-01-27 16:30:58 +0200

    First commit

---
+++ ConsoleApp1Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+using System;
+
+[TestFixture]
+[AllureNUnit]
+[AllureDisplayIgnored]
+class TestClass
+{
+    [Test(Description = "XXX")]
+    [AllureTag("Regression")]
+    [AllureSeverity(SeverityLevel.critical)]
+    [AllureIssue("ISSUE-1")]
+    [AllureTms("TMS-12")]
+    [AllureOwner("User")]
+    [AllureSuite("PassedSuite")]
+    [AllureSubSuite("NoAssert")]
+    public void TestSample()
+    {
+        Console.WriteLine(DateTime.Now);
+    }
+}
